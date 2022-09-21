397a37e04253a39224bb1d1dcfb22cb155c32646
Adam Soltys <asoltys@gmail.com>
2022-09-21 13:40:45 -0700

    set rates

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -21,6 +21,8 @@
       headers: { "content-type": "application/json" },
     }).then((r) => r.json());
 
+    props.rates = rates;
+
     let authRequired = [/a\/create/, /edit/, /wallet/, /settings/];
     if (!session?.user && authRequired.find((p) => url.pathname.match(p))) {
       return {
